e9b3c1de50a8d665a2d17279fda37bcb4a0274d2
adithyanst <pampana.adithya2024@nst.rishihood.edu.in>
2025-11-13 02:57:18 +0530

    feat: signup page setup

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
+import SignUp from "./routes/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   </BrowserRouter>,
 );
---
+++ app/src/routes/SignUp.jsx
@@ -0,0 +1,11 @@
+function SignUp() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <div className="flex items-center justify-center rounded-[12px] border-[#515151] border-[1.5px] border-solid bg-[#1B1B1B] px-12 py-6 text-white">
+        signup page
+      </div>
+    </main>
+  );
+}
+
+export default SignUp;
